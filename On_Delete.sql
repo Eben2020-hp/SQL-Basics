c6baec7c22f6cec9791c196685532dae3fde245c
Eben Emmanuel <ebenemmanuel7@gmail.com>
2022-05-10 16:57:33 +0530

    Committed

---
+++ On_Delete.sql
@@ -0,0 +1,11 @@
+--------------------- ON DELETE ---------------------
+-- DELETE AN ENTRY FROM A TABLE
+DELETE FROM employee WHERE emp_id = 102;
+SELECT * FROM branch;
+SELECT * FROM employee;
+        --- All the values where ON DELETE SET NULL is given we get the values as NULL when we remove an observation.
+        
+
+DELETE FROM branch WHERE branch_id = 2;
+SELECT * FROM branch_supplier;
+SELECT * FROM branch;
